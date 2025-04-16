e35136d3d6fb1a7a799a15835c58a43f49ec1bae
Seonwhee-Genome <quarklep@naver.com>
2025-04-16 14:46:50 +0900

    Merge MediaPipe detection and DeepFace Recognition

--- kface.py
+++ kface.py
@@ -1,13 +1,54 @@
-import cv2, os
+import cv2, os, yaml
 import matplotlib.pyplot as plt
+import numpy as np
+import mediapipe as mp
 from glob import glob
 from deepface import DeepFace
-import numpy as np
-import yaml
+
 
 class Face_img:
     def __init__(self, id):
-        self.id = id  
+        self.id = id
+        self.crop_dir = '/data/Face_id/MediaPipe/data_prep/tmp/'
+
+    def MediaPipe_face_detection(self):
+        mp_face_detection = mp.solutions.face_detection
+
+        with mp_face_detection.FaceDetection(
+            model_selection=1, min_detection_confidence=0.5) as face_detection:
+            self.image = cv2.imread(self.file_path)
+            
+            # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
+            results = face_detection.process(cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB))
+
+            # Crop and save face detections.
+            if results.detections:
+                self.crop_save_detected_bbox(results)
+                
+
+    def crop_save_detected_bbox(self, results, save=True):
+        
+        height, width, _ = self.image.shape
+        for i, detection in enumerate(results.detections):
+            # Get bounding box in absolute pixel values
+            bbox = detection.location_data.relative_bounding_box
+            x_min = int(bbox.xmin * width)
+            y_min = int(bbox.ymin * height)
+            box_width = int(bbox.width * width)
+            box_height = int(bbox.height * height)
+
+            # Make sure box stays within image bounds
+            x_min = max(0, x_min)
+            y_min = max(0, y_min)
+            x_max = min(width, x_min + box_width)
+            y_max = min(height, y_min + box_height)
+
+            # Crop and save the face region
+            self.cropped_face = self.image[y_min:y_max, x_min:x_max]
+
+            if save:
+                self.crop_path = f'{self.crop_dir}face_{i}.jpg'
+                cv2.imwrite(self.crop_path, self.cropped_face)
 
 
 class KFace:

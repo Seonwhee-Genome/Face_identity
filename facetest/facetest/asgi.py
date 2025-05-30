bac90d2087b2aa4cab379d2a5c7815c01e30fc83
Seonwhee-Genome <quarklep@naver.com>
2025-05-30 11:45:28 +0900

    correct the project name

--- facetest/facetest/asgi.py
+++ facetest/facetest/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for faceserve project.
+ASGI config for facetest project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faceserve.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facetest.settings')
 
 application = get_asgi_application()
